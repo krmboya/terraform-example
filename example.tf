390cf29d6d7ea08c54d838c8809dfbbdab438910
Kenny Rachuonyo <krmboya@gmail.com>
2020-03-25 14:54:47 +0300

    demonstrate variables and outputs

--- example.tf
+++ example.tf
@@ -1,35 +1,13 @@
 provider "aws" {
-  profile = "krmboya"
-  region  = "us-west-2"
-}
-
-resource "aws_key_pair" "example" {
-  key_name   = "examplekey"
-  public_key = file("terraform.pub")
+  profile = var.profile
+  region  = var.region
 }
 
 resource "aws_instance" "example" {
-  key_name      = aws_key_pair.example.key_name
-  ami           = "ami-04590e7389a6e577c"
+  ami           = var.amis[var.region]
   instance_type = "t2.micro"
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("terraform")
-    host        = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo amazon-linux-extras enable nginx1.12",
-      "sudo yum -y install nginx",
-      "sudo systemctl start nginx"
-    ]
-  }
 }
 
-resource "aws_eip" "ip" {
-  vpc      = true
-  instance = aws_instance.example.id
+output "ami" {
+  value = aws_instance.example.ami
 }
