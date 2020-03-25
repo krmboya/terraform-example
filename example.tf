019030bd945744161b62b22e80d043cbf919b53b
Kenny Rachuonyo <krmboya@gmail.com>
2020-03-25 14:34:03 +0300

    demonstrate remote provisioning

--- example.tf
+++ example.tf
@@ -1,24 +1,35 @@
 provider "aws" {
   profile = "krmboya"
-  region  = "us-east-1"
+  region  = "us-west-2"
+}
+
+resource "aws_key_pair" "example" {
+  key_name   = "examplekey"
+  public_key = file("terraform.pub")
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-b374d5a5"
+  key_name      = aws_key_pair.example.key_name
+  ami           = "ami-04590e7389a6e577c"
   instance_type = "t2.micro"
-  depends_on    = [aws_s3_bucket.example]
 
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("terraform")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras enable nginx1.12",
+      "sudo yum -y install nginx",
+      "sudo systemctl start nginx"
+    ]
   }
 }
 
 resource "aws_eip" "ip" {
   vpc      = true
   instance = aws_instance.example.id
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "tf-guide-somerandomsuffixheredjfldjafjdfdjfdlj"
-  acl    = "private"
 }
