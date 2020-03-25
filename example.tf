39b3c653f7daa8e4d371b2d4c86dd25718c4a873
Kenny Rachuonyo <krmboya@gmail.com>
2020-03-25 13:01:23 +0300

    demonstrate resource dependencies

--- example.tf
+++ example.tf
@@ -4,11 +4,22 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-b374d5a5"
+  ami           = "ami-2757f631"
   instance_type = "t2.micro"
+  depends_on    = [aws_s3_bucket.example]
 }
 
 resource "aws_eip" "ip" {
   vpc      = true
   instance = aws_instance.example.id
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "tf-guide-somerandomsuffixheredjfldjafjdfdjfdlj"
+  acl    = "private"
+}
+
+resource "aws_instance" "another" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
 }
